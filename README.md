802b1d2151d2820bfb1926f7956d5c15188db0dc
Guriido <christian.hamelain@laposte.net>
2020-10-12 14:31:48 +0900

    Separate scripts and add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# vim-setup
+
+## Installation steps
+
+- execute script 1
+- launch vim and execute `:PluginInstall`
+- execute script 2
---
+++ install_script_part2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install YouCompleteMe
+sudo apt-get install build-essential cmake python3-dev
+cd ~/.vim/bundle/YouCompleteMe 
+python3 install.py
+
